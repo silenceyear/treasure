28d835ec93fb65dc56a75a6b1a7b290684f27cc7
yeqi <597019425@qq.com>
2019-02-20 11:25:49 +0800

    mipush

---
+++ android/app/src/main/java/com/year/treasure/App.java
@@ -0,0 +1,78 @@
+package com.year.treasure;
+
+import android.app.ActivityManager;
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.xiaomi.channel.commonutils.logger.LoggerInterface;
+import com.xiaomi.mipush.sdk.Logger;
+import com.xiaomi.mipush.sdk.MiPushClient;
+
+import java.util.List;
+
+/**
+ * 1、为了打开客户端的日志，便于在开发过程中调试，需要自定义一个 Application。
+ * 并将自定义的 application 注册在 AndroidManifest.xml 文件中。<br/>
+ * 2、为了提高 push 的注册率，您可以在 Application 的 onCreate 中初始化 push。你也可以根据需要，在其他地方初始化 push。
+ *
+ * @author wangkuiwei
+ */
+public class App extends Application {
+    // user your appid the key.
+    private static final String APP_ID = "2882303761517947895";
+    // user your appid the key.
+    private static final String APP_KEY = "5861794712895";
+
+    // 此TAG在adb logcat中检索自己所需要的信息， 只需在命令行终端输入 adb logcat | grep
+    // com.xiaomi.mipushdemo
+    public static final String TAG = "com.year.treasure.App";
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 注册push服务，注册成功后会向DemoMessageReceiver发送广播
+        // 可以从DemoMessageReceiver的onCommandResult方法中MiPushCommandMessage对象参数中获取注册信息
+        if (shouldInit()) {
+            MiPushClient.registerPush(this, APP_ID, APP_KEY);
+        }
+
+        LoggerInterface newLogger = new LoggerInterface() {
+
+            @Override
+            public void setTag(String tag) {
+                // ignore
+            }
+
+            @Override
+            public void log(String content, Throwable t) {
+                Log.d(TAG, content, t);
+            }
+
+            @Override
+            public void log(String content) {
+                Log.d(TAG, content);
+            }
+        };
+        Logger.setLogger(this, newLogger);
+    }
+
+    private boolean shouldInit() {
+        ActivityManager am = ((ActivityManager) getSystemService(Context.ACTIVITY_SERVICE));
+        List<ActivityManager.RunningAppProcessInfo> processInfos = am.getRunningAppProcesses();
+        String mainProcessName = getPackageName();
+        int myPid = android.os.Process.myPid();
+        for (ActivityManager.RunningAppProcessInfo info : processInfos) {
+            if (info.pid == myPid && mainProcessName.equals(info.processName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void reInitPush(Context ctx) {
+        MiPushClient.registerPush(ctx.getApplicationContext(), APP_ID, APP_KEY);
+    }
+}
---
+++ android/app/src/main/java/com/year/treasure/broadcast/MiPushMessageReceiver.java
@@ -0,0 +1,155 @@
+package com.year.treasure.broadcast;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.xiaomi.mipush.sdk.ErrorCode;
+import com.xiaomi.mipush.sdk.MiPushClient;
+import com.xiaomi.mipush.sdk.MiPushCommandMessage;
+import com.xiaomi.mipush.sdk.MiPushMessage;
+import com.xiaomi.mipush.sdk.PushMessageReceiver;
+import com.year.treasure.App;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MiPushMessageReceiver extends PushMessageReceiver {
+    private String mRegId;
+    private String mTopic;
+    private String mAlias;
+    private String mAccount;
+    private String mStartTime;
+    private String mEndTime;
+
+    @Override
+    public void onReceivePassThroughMessage(Context context, MiPushMessage message) {
+        Log.v(App.TAG,
+                "onReceivePassThroughMessage is called. " + message.toString());
+
+        if (!TextUtils.isEmpty(message.getTopic())) {
+            mTopic = message.getTopic();
+        } else if (!TextUtils.isEmpty(message.getAlias())) {
+            mAlias = message.getAlias();
+        }
+
+        Message msg = Message.obtain();
+    }
+
+    @Override
+    public void onNotificationMessageClicked(Context context, MiPushMessage message) {
+        Log.v(App.TAG,
+                "onNotificationMessageClicked is called. " + message.toString());
+
+        if (!TextUtils.isEmpty(message.getTopic())) {
+            mTopic = message.getTopic();
+        } else if (!TextUtils.isEmpty(message.getAlias())) {
+            mAlias = message.getAlias();
+        }
+
+    }
+
+    @Override
+    public void onNotificationMessageArrived(Context context, MiPushMessage message) {
+        Log.v(App.TAG,
+                "onNotificationMessageArrived is called. " + message.toString());
+
+        if (!TextUtils.isEmpty(message.getTopic())) {
+            mTopic = message.getTopic();
+        } else if (!TextUtils.isEmpty(message.getAlias())) {
+            mAlias = message.getAlias();
+        }
+
+        Message msg = Message.obtain();
+    }
+
+    @Override
+    public void onCommandResult(Context context, MiPushCommandMessage message) {
+        Log.v(App.TAG,
+                "onCommandResult is called. " + message.toString());
+        String command = message.getCommand();
+        List<String> arguments = message.getCommandArguments();
+        String cmdArg1 = ((arguments != null && arguments.size() > 0) ? arguments.get(0) : null);
+        String cmdArg2 = ((arguments != null && arguments.size() > 1) ? arguments.get(1) : null);
+        String log;
+        if (MiPushClient.COMMAND_REGISTER.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mRegId = cmdArg1;
+            } else {
+            }
+        } else if (MiPushClient.COMMAND_SET_ALIAS.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mAlias = cmdArg1;
+            } else {
+            }
+        } else if (MiPushClient.COMMAND_UNSET_ALIAS.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mAlias = cmdArg1;
+            } else {
+            }
+        } else if (MiPushClient.COMMAND_SET_ACCOUNT.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mAccount = cmdArg1;
+            } else {
+            }
+        } else if (MiPushClient.COMMAND_UNSET_ACCOUNT.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mAccount = cmdArg1;
+            } else {
+            }
+        } else if (MiPushClient.COMMAND_SUBSCRIBE_TOPIC.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mTopic = cmdArg1;
+            } else {
+            }
+        } else if (MiPushClient.COMMAND_UNSUBSCRIBE_TOPIC.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mTopic = cmdArg1;
+            } else {
+            }
+        } else if (MiPushClient.COMMAND_SET_ACCEPT_TIME.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mStartTime = cmdArg1;
+                mEndTime = cmdArg2;
+            } else {
+            }
+        } else {
+            log = message.getReason();
+        }
+
+        Message msg = Message.obtain();
+    }
+
+    @Override
+    public void onReceiveRegisterResult(Context context, MiPushCommandMessage message) {
+        Log.v(App.TAG,
+                "onReceiveRegisterResult is called. " + message.toString());
+        String command = message.getCommand();
+        List<String> arguments = message.getCommandArguments();
+        String cmdArg1 = ((arguments != null && arguments.size() > 0) ? arguments.get(0) : null);
+        String log;
+        if (MiPushClient.COMMAND_REGISTER.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mRegId = cmdArg1;
+            } else {
+            }
+        } else {
+            log = message.getReason();
+        }
+
+        Message msg = Message.obtain();
+    }
+
+    @Override
+    public void onRequirePermissions(Context context, String[] permissions) {
+        super.onRequirePermissions(context, permissions);
+        Log.e(App.TAG,
+                "onRequirePermissions is called. need permission" + Arrays.toString(permissions));
+
+        if (Build.VERSION.SDK_INT >= 23 && context.getApplicationInfo().targetSdkVersion >= 23) {
+        }
+    }
+
+}
